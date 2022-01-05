a71553f20b2d4000d85cf8c1bb105c6f75bd65f0
whil <miltonivan117@gmail.com>
2022-01-05 16:22:04 -0600

    delete sale

--- src/routers/listsales/listsales.ts
+++ src/routers/listsales/listsales.ts
@@ -40,4 +40,27 @@ router.get('/listsales', async (req: Request, res: Response) => {
     });
   }
 });
+
+router.delete('/listsales/item', async (req: Request, res: Response) => {
+  const token: string | any = req.headers['token'];
+  if (!token) {
+    return res.status(401).send({ auth: false, message: 'No token provided.' });
+  }
+  const verifyToken: { _id: string } | any = jwt.verify(token, KeyJwt());
+  if (!verifyToken) {
+    res.status(405).json({
+      error: 405,
+      message: 'Token is invalid'
+    });
+  }
+  try {
+    await NewSale.findByIdAndDelete(req.body.id);
+    res.status(200).json({
+      message: {
+        title: 'Delete success',
+        text: 'Delete an sale successfull'
+      }
+    });
+  } catch (error) {}
+});
 module.exports = router;
